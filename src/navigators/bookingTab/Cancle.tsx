4ed9098c51f460129a34791e7fd253c8a05f9ce5
Mehran Afridi <60032751+mehranafridii@users.noreply.github.com>
2024-07-30 05:35:47 +0500

    feat: Cancelled Tab UI and Data integration flow setting

--- src/navigators/bookingTab/Cancle.tsx
+++ src/navigators/bookingTab/Cancle.tsx
@@ -1,18 +1,24 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import ProfileDetailBox from '../../components/ProfileDetailbox/ProfileDetailBox';
 import strings from '../../utils/strings/strings';
 import {Images} from '../../assets/images';
 import {Colors} from '../../utils/colors/colors';
 
-const Cancle = () => {
-  return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+const Cancle = ({data}: any) => {
+  if (!data) return null;
+  const renderItem = ({item, index}) => {
+    return (
       <ProfileDetailBox
+        itemData={item}
         dateText={strings.aug_25}
         image={Images.addImage}
-        hideToggle={true}
       />
+    );
+  };
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <FlatList data={data} renderItem={renderItem} />
     </ScrollView>
   );
 };
