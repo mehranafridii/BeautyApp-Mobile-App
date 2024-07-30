664d162ceccfc214f712970a1321d8dc0eeebe8e
Mehran Afridi <60032751+mehranafridii@users.noreply.github.com>
2024-07-30 05:39:13 +0500

    feat:GetUpcoming,GetCompleted, GetCancel , GetBookings API configuration setting for Customer and Artist

---
+++ src/Redux/services/app/AppApi.ts
@@ -0,0 +1,129 @@
+import {Endpoints} from '../../../config/Endpoints';
+import {apiMethods, baseApi} from '../api';
+
+const customerBookingService = (body: any) => {
+  return {
+    url: Endpoints.customerBookingService,
+    method: apiMethods.post,
+    body,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const customerUpcomingBookings = () => {
+  return {
+    url: Endpoints.customerUpcomingBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const artistUpcomingBookings = () => {
+  return {
+    url: Endpoints.artistUpcomingBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const customerCompleteBookings = () => {
+  return {
+    url: Endpoints.customerCompleteBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const artistCompleteBookings = () => {
+  return {
+    url: Endpoints.artistCompleteBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const customerCancelBookings = () => {
+  return {
+    url: Endpoints.customerCancelBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const artistCancelBookings = () => {
+  return {
+    url: Endpoints.artistCancelBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const customerBookings = () => {
+  return {
+    url: Endpoints.customerBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+const artistBookings = () => {
+  return {
+    url: Endpoints.artistBookings,
+    method: apiMethods.get,
+    headers: {
+      'Content-type': 'application/json',
+    },
+  };
+};
+////
+export const AuthApi = baseApi.injectEndpoints({
+  endpoints: build => ({
+    customerBookingService: build.mutation({
+      query: customerBookingService,
+    }),
+    customerUpcomingBookings: build.query({
+      query: customerUpcomingBookings,
+    }),
+    artistUpcomingBookings: build.query({
+      query: artistUpcomingBookings,
+    }),
+    customerCompleteBookings: build.query({
+      query: customerCompleteBookings,
+    }),
+    artistCompleteBookings: build.query({
+      query: artistCompleteBookings,
+    }),
+    customerCancelBookings: build.query({
+      query: customerCancelBookings,
+    }),
+    artistCancelBookings: build.query({
+      query: artistCancelBookings,
+    }),
+    customerBookings: build.query({
+      query: customerBookings,
+    }),
+    artistBookings: build.query({
+      query: artistBookings,
+    }),
+  }),
+});
+
+export const {
+  useCustomerBookingServiceMutation,
+  useLazyArtistBookingsQuery,
+  useLazyArtistUpcomingBookingsQuery,
+  useLazyArtistCompleteBookingsQuery,
+  useLazyArtistCancelBookingsQuery,
+  useLazyCustomerBookingsQuery,
+  useLazyCustomerCancelBookingsQuery,
+  useLazyCustomerCompleteBookingsQuery,
+  useLazyCustomerUpcomingBookingsQuery,
+} = AuthApi;
--- src/config/Endpoints.ts
+++ src/config/Endpoints.ts
@@ -1,8 +1,19 @@
 export const Endpoints = {
+  //AUTH
   login: 'login',
   signUp: 'register_user',
   signUpArtist: 'register_artist',
   forgotPassword: 'users/forget-password',
   verifyOTP: 'users/otp-verification',
   resetPassword: 'users/reset-password',
+  //APP
+  customerBookingService: 'Booking/Customer/Store',
+  customerUpcomingBookings: 'Booking/Customer/Activebooking',
+  artistUpcomingBookings: 'Booking/Artist/Activebooking',
+  customerCompleteBookings: 'Booking/Customer/Completebooking',
+  artistCompleteBookings: 'Booking/Artist/Completebooking',
+  customerCancelBookings: 'Booking/Customer/Cancelbooking',
+  artistCancelBookings: 'Booking/Artist/Cancelbooking',
+  customerBookings: 'Booking/Customer/Bookings',
+  artistBookings: 'Booking/Artist/Bookings',
 };
