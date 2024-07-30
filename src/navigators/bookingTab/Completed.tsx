e2b56c86ac60454fb032490886c51187a9518631
Mehran Afridi <60032751+mehranafridii@users.noreply.github.com>
2024-07-30 05:36:05 +0500

    feat: Completed Tab UI and Data integration flow setting

--- src/navigators/bookingTab/Completed.tsx
+++ src/navigators/bookingTab/Completed.tsx
@@ -1,18 +1,26 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {Colors} from '../../utils/colors/colors';
 import ProfileDetailBox from '../../components/ProfileDetailbox/ProfileDetailBox';
 import strings from '../../utils/strings/strings';
 import {Images} from '../../assets/images';
 
-const Completed = () => {
-  return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+const Completed = ({data}: any) => {
+  console.log(data, 'CompletedData');
+  if (!data) return null;
+
+  const renderItem = ({item, index}) => {
+    return (
       <ProfileDetailBox
+        itemData={item}
         dateText={strings.aug_25}
         image={Images.addImage}
-        hideToggle={true}
       />
+    );
+  };
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <FlatList data={data} renderItem={renderItem} />
     </ScrollView>
   );
 };
