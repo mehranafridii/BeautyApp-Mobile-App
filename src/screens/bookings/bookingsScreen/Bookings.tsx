a5126037dbcc0f4da06777426d7ad6f119760de0
Mehran Afridi <60032751+mehranafridii@users.noreply.github.com>
2024-07-30 05:40:00 +0500

    feat: GetUpcoming,GetCompleted, GetCancel , GetBookings API Implementaion at Booking Screen for Both Customer and Artist

--- src/screens/bookings/bookingsScreen/Bookings.tsx
+++ src/screens/bookings/bookingsScreen/Bookings.tsx
@@ -1,5 +1,5 @@
-import {Dimensions, StyleSheet, View} from 'react-native';
-import React from 'react';
+import {Alert, Dimensions, StyleSheet, View} from 'react-native';
+import React, {useEffect} from 'react';
 import Header from '../../../components/header/Header';
 import strings from '../../../utils/strings/strings';
 import {Colors} from '../../../utils/colors/colors';
@@ -8,20 +8,177 @@ import Upcoming from '../../../navigators/bookingTab/Upcoming';
 import Ongoing from '../../../navigators/bookingTab/Ongoing';
 import Completed from '../../../navigators/bookingTab/Completed';
 import Cancle from '../../../navigators/bookingTab/Cancle';
+import {useSelector} from 'react-redux';
+import {getUserType} from '../../../Redux/Reducers/UserTypeSlice';
+import {
+  useLazyArtistBookingsQuery,
+  useLazyArtistCancelBookingsQuery,
+  useLazyArtistCompleteBookingsQuery,
+  useLazyArtistUpcomingBookingsQuery,
+  useLazyCustomerBookingsQuery,
+  useLazyCustomerCancelBookingsQuery,
+  useLazyCustomerCompleteBookingsQuery,
+  useLazyCustomerUpcomingBookingsQuery,
+} from '../../../Redux/services/app/AppApi';
 
 const Bookings = () => {
+  //API initialization
+  const [customerUpcomingApi, {data: customerUpcomingData}] =
+    useLazyCustomerUpcomingBookingsQuery();
+  const [customerCompletedApi, {data: customerCompletedData}] =
+    useLazyCustomerCompleteBookingsQuery();
+  const [customerCancelApi, {data: customerCancelData}] =
+    useLazyCustomerCancelBookingsQuery();
+  const [customerBookingApi, {data: customerBookingData}] =
+    useLazyCustomerBookingsQuery();
+
+  const [artistUpcomingApi, {data: artistUpcomingData}] =
+    useLazyArtistUpcomingBookingsQuery();
+  const [artistCompletedApi, {data: artistCompletedData}] =
+    useLazyArtistCompleteBookingsQuery();
+  const [artistCancelApi, {data: artistCancelData}] =
+    useLazyArtistCancelBookingsQuery();
+  const [artistBookingApi, {data: artistBookingData}] =
+    useLazyArtistBookingsQuery();
+  //Store
+  const userType = useSelector(getUserType);
+  //Consts
+  const isBusiness = userType === 'business' ? true : false;
+  // States
   const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
+  const [artistRoutes] = React.useState([
     {key: 'first', title: strings.upcoming},
     {key: 'second', title: strings.completed},
     {key: 'third', title: strings.onGoing},
     {key: 'fourth', title: strings.cancle},
   ]);
-  const renderScene = SceneMap({
-    first: Upcoming,
-    second: Ongoing,
-    third: Completed,
-    fourth: Cancle,
+  const [customerRoutes] = React.useState([
+    {key: 'first', title: strings.upcoming},
+    {key: 'second', title: strings.completed},
+    {key: 'third', title: strings.cancle},
+  ]);
+
+  /// APIs
+  useEffect(() => {
+    // Check Type and HIT APIs
+    if (isBusiness) {
+      index === 0
+        ? GetArtistUpcomingBookingAPI()
+        : index === 1
+        ? GetArtistCompletedBookingAPI()
+        : index === 2
+        ? GetArtistBookingAPI()
+        : GetArtistCancelBookingAPI();
+    } else {
+      index === 0
+        ? GetCustomerUpcomingBookingAPI()
+        : index === 1
+        ? GetCustomerCompeletedBookingAPI()
+        : GetCustomerCancelBookingAPI();
+    }
+  }, [index]);
+
+  ///
+
+  /// API functions
+  //API Functions
+
+  // Artist-Business APIs below
+  const GetArtistUpcomingBookingAPI = () => {
+    artistUpcomingApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetArtistCompletedBookingAPI = () => {
+    artistCompletedApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetArtistBookingAPI = () => {
+    artistBookingApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetArtistCancelBookingAPI = () => {
+    artistCancelApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  ///////////////////////////////////////////
+  // Customers APIs below
+  const GetCustomerUpcomingBookingAPI = () => {
+    customerUpcomingApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'dfdsCUSOTME11');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetCustomerCompeletedBookingAPI = () => {
+    customerCompletedApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetCustomerCancelBookingAPI = () => {
+    Alert.alert(JSON.stringify(index));
+    customerCancelApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  const GetCustomerBookingAPI = () => {
+    customerBookingApi('')
+      .unwrap()
+      ?.then(res => {
+        console.log(res, 'RESDFD');
+      })
+      .catch(error => {
+        console.log(error, 'sdjfkdsjfk');
+      });
+  };
+  ///////////////////////////////////////////////
+  // UI functions below
+  const renderCustomerScene = SceneMap({
+    first: () => <Upcoming data={customerUpcomingData?.data} />,
+    second: () => <Completed data={customerCompletedData?.data} />,
+    third: () => <Cancle data={customerCancelData?.data} />,
+  });
+  const renderArtistScene = SceneMap({
+    first: () => <Upcoming data={artistUpcomingData?.data} />,
+    second: () => <Ongoing data={artistBookingData?.data} />,
+    third: () => <Completed data={artistCompletedData?.data} />,
+    fourth: () => <Cancle data={artistCancelData?.data} />,
   });
   const renderTabBar = (props: any) => (
     <TabBar
@@ -31,11 +188,16 @@ const Bookings = () => {
       style={{backgroundColor: Colors.white}}
     />
   );
+  // User TYPE RENDERING CONDITIONS
+  const Routes = isBusiness ? artistRoutes : customerRoutes;
+  const renderScene = isBusiness ? renderArtistScene : renderCustomerScene;
+
+  // MAIN RETURN
   return (
     <View style={styles.container}>
       <Header heading={strings?.booking} searchCircle={true} />
       <TabView
-        navigationState={{index, routes}}
+        navigationState={{index, routes: Routes}}
         renderScene={renderScene}
         onIndexChange={setIndex}
         renderTabBar={renderTabBar}
