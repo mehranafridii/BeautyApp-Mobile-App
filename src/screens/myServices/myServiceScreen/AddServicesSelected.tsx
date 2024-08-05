bb070f7c59ce2200f31e36ea00a4bcbdc54bcf61
Mehran Afridi <60032751+mehranafridii@users.noreply.github.com>
2024-08-05 12:04:35 +0500

    fix: Add Sevice UI updated and Assets are updated for Category

--- src/screens/myServices/myServiceScreen/AddServicesSelected.tsx
+++ src/screens/myServices/myServiceScreen/AddServicesSelected.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import React from 'react';
 import Header from '../../../components/header/Header';
 import strings from '../../../utils/strings/strings';
@@ -7,8 +7,20 @@ import CustomText from '../../../components/text/CustomText';
 import CircleImage from '../../../components/CircleImage/CircleImage';
 import CustomButton from '../../../components/button/CustomButton';
 import {screenHeight, screenWidth} from '../../../utils/dimensions';
+import {Images} from '../../../assets/images';
 
 const AddServicesSelected = () => {
+  const categoriesData = [
+    {categoryTitle: strings.haircut, image: Images.hairCuts},
+    {categoryTitle: strings.makeup, image: Images.makeUp},
+    {categoryTitle: strings.shaving, image: Images.shavingBeard},
+    {categoryTitle: strings.hairdry, image: Images.hairDry},
+    {categoryTitle: strings.massage, image: Images.massage},
+    {categoryTitle: strings.henna, image: Images.henna},
+    {categoryTitle: strings.nailcare, image: Images.nailCare},
+    {categoryTitle: strings.feetcare, image: Images.feetCare},
+    {categoryTitle: strings.skincare, image: Images.skinCare},
+  ];
   return (
     <View style={styles.container}>
       <Header heading={strings?.addService} />
@@ -18,27 +30,15 @@ const AddServicesSelected = () => {
           style={styles.serviceText}
           size={14}
         />
-        <View style={styles.rowCon}>
-          <CircleImage text={strings.haircut} />
-          <CircleImage text={strings.makeup} />
-          <CircleImage text={strings.shaving} />
-        </View>
-        <View style={styles.rowCon}>
-          <CircleImage text={strings.hairdry} />
-          <CircleImage text={strings.massage} />
-          <CircleImage text={strings.henna} />
-        </View>
-        <View style={styles.rowCon}>
-          <CircleImage text={strings.nailcare} />
-          <CircleImage text={strings.feetcare} />
-          <CircleImage text={strings.skincare} />
-        </View>
-        <View style={{marginTop: screenHeight / 10}}>
-          <CustomButton
-            text={strings.next}
-            style={{width: screenWidth / 1.1}}
-          />
-        </View>
+        <FlatList
+          data={categoriesData}
+          horizontal={false}
+          numColumns={3}
+          columnWrapperStyle={styles.columnStyle}
+          renderItem={({item}) => (
+            <CircleImage text={item?.categoryTitle} image={item?.image} />
+          )}
+        />
       </View>
     </View>
   );
@@ -48,10 +48,11 @@ export default AddServicesSelected;
 
 const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: Colors.white},
+  columnStyle: {justifyContent: 'space-between'},
   rowCon: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-  serviceText: {textAlign: 'left'},
-  innerContainer: {padding: 15},
+  serviceText: {textAlign: 'left', paddingHorizontal: 5},
+  innerContainer: {paddingHorizontal: 15},
 });
